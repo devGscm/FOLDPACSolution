72deed9e39245fe873472009e25fddbf99317c6b
이용민 <yomile@iyongmin-ui-iMac.local>
2017-11-17 11:15:24 +0900

    sss

--- MosaicTests/MosaicTests.swift
+++ MosaicTests/MosaicTests.swift
@@ -17,7 +17,7 @@ class MosaicTests: XCTestCase {
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        // xxxx teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
