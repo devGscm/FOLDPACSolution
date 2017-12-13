9a5f2b16f93c79150527e8291cbddec2ce3ab19d
이용민 <yomile@msn.com>
2017-12-13 12:36:38 +0900

    재고실사

---
+++ RRPPClient/Stock/StockReviewSearchCell.swift
@@ -0,0 +1,28 @@
+//
+//  StockReviewSearchCell.swift
+//   RRPPClient
+//
+//  Created by 이용민 on 2017. 12. 13..
+//  Copyright © 2017년 MORAMCNT. All rights reserved.
+//
+
+import UIKit
+
+class StockReviewSearchCell: UITableViewCell
+{
+	@IBOutlet weak var lblStockReviewId: UILabel!
+	@IBOutlet weak var lblReviewReqDate: UILabel!
+	@IBOutlet weak var lblProdAssetEpcName: UILabel!
+	@IBOutlet weak var btnSelection: UIButton!
+
+	override func awakeFromNib()
+	{
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool)
+	{
+        super.setSelected(selected, animated: animated)
+    }
+
+}
